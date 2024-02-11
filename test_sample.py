b0d2923ed9f5f34d6f56bd436e65025dbfa0b9a9
PrasannaYelsan <72689004+PrasannaYelsan@users.noreply.github.com>
2024-02-12 01:15:25 +0530

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -1,6 +1,6 @@
 import add
 import pytest
 
-@pytest.mark.parametrize("inp1, inp2, exp", [[2, 3, "positive"]])
+@pytest.mark.parametrize("inp1, inp2, exp", [[2, 3, "positive"], [-3,-2,"negative"]])
 def test_return_value(inp1, inp2, exp):
     assert add.find_sign(inp1, inp2) == exp
